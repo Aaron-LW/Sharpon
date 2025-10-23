bfcc88e900449c469df20128df8dfe16e8293218
Aaron_LW <aaronl.waechter@gmail.com>
2025-10-23 15:52:51 +0200

    Added multiple lines

--- Sharpon/Code/UISystem/UISystem.cs
+++ Sharpon/Code/UISystem/UISystem.cs
@@ -5,6 +5,7 @@ using FontStashSharp;
 using System.IO;
 using System;
 using MonoGame.Extended.Graphics;
+using System.Collections.Generic;
 
 public static class UISystem
 {
@@ -14,7 +15,10 @@ public static class UISystem
     private static FontSystem _fontSystem = new FontSystem();
     private static GameWindow _gameWindow;
     private static SimpleFps _fps = new SimpleFps();
-    private static string _text;
+    private static float _lineSpacing => (float)(1 * BaseFontSize);
+
+    public static List<string> Lines = new List<string>() { "" };
+    public static int LineIndex = 0;
         
     public static void Start(GameWindow gameWindow)
     {
@@ -44,14 +48,44 @@ public static class UISystem
             }
         }
 
-        _text = Input.GetPressedKeys(_text);
+        if (Input.IsKeyPressed(Keys.Down))
+        {
+            if (LineIndex < Lines.Count - 1)
+            {
+                LineIndex++;
+            }
+        }
+
+        if (Input.IsKeyPressed(Keys.Up))
+        {
+            if (LineIndex > 0)
+            {
+                LineIndex--;
+            }
+        }
+
+        if (Input.IsKeyPressed(Keys.Enter))
+        {
+            Lines.Insert(LineIndex + 1, "");
+            LineIndex++;
+        }
+
+        Lines[LineIndex] = Input.GetPressedKeys(Lines[LineIndex]);
     }
     
     public static void Draw(SpriteBatch spriteBatch)
     {
         SpriteFontBase font = _fontSystem.GetFont(BaseFontSize * ScaleModifier);
-        spriteBatch.DrawString(font, _text, new Vector2(100, 100) * ScaleModifier, Color.White);
-        spriteBatch.DrawString(font, _fps.msg, new Vector2(50, 50) * ScaleModifier, Color.White);
+
+        for (int i = 0; i < Lines.Count; i++)
+        {
+            spriteBatch.DrawString(font, Lines[i], new Vector2(100, 100 + (i * _lineSpacing)) * ScaleModifier, Color.White);
+        }
+
+        spriteBatch.DrawString(font, _fps.msg, new Vector2(20, 20) * ScaleModifier, Color.White);
         _fps.frames++;
+
+        spriteBatch.DrawString(font, $"Lines: {Lines.Count}", new Vector2(150, 20) * ScaleModifier, Color.White);
+        spriteBatch.DrawString(font, $"Current Line: {LineIndex}", new Vector2(250, 20) * ScaleModifier, Color.White);
     }
 }
